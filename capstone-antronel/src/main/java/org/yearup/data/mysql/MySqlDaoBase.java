f810475c787a16efc8a4635b86ac4af7174ee0d3
Antronel <antronelbienaime@yahoo.com>
2025-06-27 01:33:25 -0400

    capstone-antronel

---
+++ capstone-antronel/src/main/java/org/yearup/data/mysql/MySqlDaoBase.java
@@ -0,0 +1,24 @@
+package org.yearup.data.mysql;
+
+import org.yearup.models.Product;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class MySqlDaoBase
+{
+    private DataSource dataSource;
+
+    public MySqlDaoBase(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+
+    protected Connection getConnection() throws SQLException
+    {
+        return dataSource.getConnection();
+    }
+
+    public abstract void update(int productId, Product product);
+}
